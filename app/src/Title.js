fbfd737ca2f4240ab839903d4245d7efbfd72bdd
rage997 <zuppin@usi.ch>
2021-10-07 13:10:22 +0200

    Update placeholder for TLS logo

--- app/src/Title.js
+++ app/src/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ show, setShow, content }) => {
   return <div id='title'>
       <h1>Ticino Land of Stars </h1>
-      <img src="/meme.png" alt="logo"></img>
+      <img src="images/placeholder.png" alt="logo"></img>
   </div>
   
 };
